7ee7a874f67f4033496f84a2a38658c2bf95d771
Kelly Liu <kellyliu@dhcp-18-189-23-80.dyn.mit.edu>
2015-10-20 23:01:04 -0400

    initialize pset code

---
+++ starter3/build/src/CMakeFiles/a3.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kellyliu/Documents/Senior/837/starter3/src/ClothSystem.cpp" "/Users/kellyliu/Documents/Senior/837/starter3/build/src/CMakeFiles/a3.dir/ClothSystem.cpp.o"
+  "/Users/kellyliu/Documents/Senior/837/starter3/src/TimeStepper.cpp" "/Users/kellyliu/Documents/Senior/837/starter3/build/src/CMakeFiles/a3.dir/TimeStepper.cpp.o"
+  "/Users/kellyliu/Documents/Senior/837/starter3/src/camera.cpp" "/Users/kellyliu/Documents/Senior/837/starter3/build/src/CMakeFiles/a3.dir/camera.cpp.o"
+  "/Users/kellyliu/Documents/Senior/837/starter3/src/main.cpp" "/Users/kellyliu/Documents/Senior/837/starter3/build/src/CMakeFiles/a3.dir/main.cpp.o"
+  "/Users/kellyliu/Documents/Senior/837/starter3/src/particleSystem.cpp" "/Users/kellyliu/Documents/Senior/837/starter3/build/src/CMakeFiles/a3.dir/particleSystem.cpp.o"
+  "/Users/kellyliu/Documents/Senior/837/starter3/src/pendulumSystem.cpp" "/Users/kellyliu/Documents/Senior/837/starter3/build/src/CMakeFiles/a3.dir/pendulumSystem.cpp.o"
+  "/Users/kellyliu/Documents/Senior/837/starter3/src/simpleSystem.cpp" "/Users/kellyliu/Documents/Senior/837/starter3/build/src/CMakeFiles/a3.dir/simpleSystem.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../vecmath/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/kellyliu/Documents/Senior/837/starter3/build/vecmath/CMakeFiles/vecmath.dir/DependInfo.cmake"
+  )
